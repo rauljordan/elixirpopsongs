94ae7d120d8af2fe0a2ca95d6c71161339ebb05a
Raul Jordan <raul@prysmaticlabs.com>
2020-12-06 13:38:07 -0600

    add in heroku prod configs

--- lib/citypopsongs_web/endpoint.ex
+++ lib/citypopsongs_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule CitypopsongsWeb.Endpoint do
   ]
 
   socket "/socket", CitypopsongsWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
