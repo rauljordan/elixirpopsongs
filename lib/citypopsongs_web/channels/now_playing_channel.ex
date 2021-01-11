5bf025c2e3a59eb0bb9c9921def38eb62e242965
Raul Jordan <raul@prysmaticlabs.com>
2021-01-10 23:55:32 -0600

    rem rem

--- lib/citypopsongs_web/channels/now_playing_channel.ex
+++ lib/citypopsongs_web/channels/now_playing_channel.ex
@@ -1,11 +1,9 @@
 defmodule CitypopsongsWeb.NowPlayingChannel do
   use CitypopsongsWeb, :channel
   alias Citypopsongs.Multimedia
-  require Logger
 
   @impl true
   def join("now_playing:lobby", _payload, socket) do
-    Logger.info "join join"
     {:ok, socket}
   end
 
@@ -13,11 +11,9 @@ defmodule CitypopsongsWeb.NowPlayingChannel do
   # by sending replies to requests from the client
   @impl true
   def handle_in("ping", _payload, socket) do
-    Logger.info "Pinged"
     %{track: track, start_at: start_at, end_at: end_at} =
       Multimedia.NowPlaying.get_track()
     Multimedia.increase_listens(track)
-    Logger.error "attempting ping"
     {:reply, {:ok,
       %{
         slug: track.slug,
